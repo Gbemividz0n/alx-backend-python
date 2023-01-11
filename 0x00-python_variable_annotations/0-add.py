e4512da00c584c5048c4f6e4ca8a4cb7c58d214e
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2023-01-12 00:13:21 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
-''' type-annotated function add'''
+!/usr/bin/env python3
+""" A type-annotated function add."""
 
 
 def add(a: float, b: float) -> float:
-    '''
-    Args:
-        a: float
-        b: float
-    Return:
-        their sum as a float.
-    '''
-    return a + 
+    """ Returns the sum of two floats."""
+    return a + b
+
+
+# if __name__ == "__main__":
+#     print(add(1.11, 2.22) == 1.11 + 2.22)
+#     print(add.__annotations__)
