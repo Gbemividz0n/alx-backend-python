7e36e58138dc49a660d396d05a935114af4234cf
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2023-01-12 01:08:04 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-""" A type-annotated function concat."""
+''' type-annotated function concat'''
 
 
 def concat(str1: str, str2: str) -> str:
-    """ Returns the concatenation of two strings."""
-    return "{} {}".format(str1, str2)
-
-
-# if __name__ == "__main__":
-#     print(concat("Hello", "World"))
-#     print(concat.__annotations__)
+    '''
+    Args:
+        str1: string
+        str2: string
+    Return:
+        their sum as a float.
+    '''
+    return str1 + 
